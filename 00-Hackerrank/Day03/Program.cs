0152bdd39ce8f56fe453e8fcfeafa7c7686a0a9c
Taylan Alp Muhur <satanjager@gmail.com>
2026-01-25 01:00:00 +0100

    C# devam

--- 00-Hackerrank/Day03/Program.cs
+++ 00-Hackerrank/Day03/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int N = Convert.ToInt32(Console.ReadLine().Trim());
+
+        if(N % 2 == 1 || (N >= 6 && N <= 20))
+        {
+            Console.WriteLine("Weird");
+        }
+        else
+        {
+            Console.WriteLine("Not Weird");
+           
+        }
+
     }
 }
---
+++ 08_2-switch_case/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace _08_2_switch_case;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
