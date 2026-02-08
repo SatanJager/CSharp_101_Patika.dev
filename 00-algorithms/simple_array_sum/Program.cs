0ff50116830774bfcc3c7d5fb03ceb077dd6ddc7
Taylan Alp Muhur <satanjager@gmail.com>
2026-02-08 23:52:52 +0100

    C# devam

--- 00-algorithms/simple_array_sum/Program.cs
+++ 00-algorithms/simple_array_sum/Program.cs
@@ -13,13 +13,11 @@ class Result
     public static int simpleArraySum(List<int> ar)
     {
         int sum = 0;
-        for(int i = 0; i < ar.Length; i++)
+        for(int i = 0; i < ar.Count; i++)
         {
             sum += ar[i];
         }
-
         return sum;
-
     }
 }
 
