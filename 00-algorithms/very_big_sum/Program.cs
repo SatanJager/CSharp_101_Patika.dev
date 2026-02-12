a8d6e1a5537ebb962cbe1df70b4a7d10ab47e150
Taylan Alp Muhur <satanjager@gmail.com>
2026-02-12 16:57:24 +0100

    C# Hackerrank devam

--- 00-algorithms/very_big_sum/Program.cs
+++ 00-algorithms/very_big_sum/Program.cs
@@ -1,9 +1,42 @@
 ﻿namespace very_big_sum;
 
-class Program
+class Result
 {
-    static void Main(string[] args)
+
+    /*
+     * Complete the 'aVeryBigSum' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts LONG_INTEGER_ARRAY ar as parameter.
+     */
+
+    public static long aVeryBigSum(List<long> ar)
     {
-        Console.WriteLine("Hello, World!");
+        long sum = 0;
+        foreach(var sayi in ar)  //var yada long kullanılabilinir
+        {
+            sum += sayi;
+        }
+        return sum;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int arCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<long> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt64(arTemp)).ToList();
+
+        long result = Result.aVeryBigSum(ar);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
     }
 }
