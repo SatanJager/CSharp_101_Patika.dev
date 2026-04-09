4774337875bcba0e57191238492663082c2bf4ab
Taylan Alp Muhur <satanjager@gmail.com>
2026-04-09 15:56:31 +0200

    hackerrank

---
+++ 00-algorithms/time_conversion/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace time_conversion;
+
+class Result
+{
+
+    /*
+     * Complete the 'timeConversion' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static string timeConversion(string s)
+    {
+        bool basariliDonusum = DateTime.TryParse(s, out DateTime dt);
+
+        if (basariliDonusum)
+        {
+            return dt.ToString("HH:mm:ss");
+        }
+        else
+        {
+            return "Unknown Time Format";
+        }
+        
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string s = Console.ReadLine();
+
+        string result = Result.timeConversion(s);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
