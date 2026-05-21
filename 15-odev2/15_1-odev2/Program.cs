fd521090789c98bc1d6dc4dbc3a3cec82038bf80
Taylan Alp Muhur <satanjager@gmail.com>
2026-05-21 17:34:33 +0200

    C# devam

---
+++ 15-odev2/15_1-odev2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+
+namespace _15_1_odev2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Soru - 1: 
+        // Klavyeden girilen 20 adet pozitif sayının asal ve asal olmayan olarak 2 ayrı listeye atın. (ArrayList sınıfını kullanara yazınız.)
+        // Negatif ve numeric olmayan girişleri engelleyin.
+        // Her bir dizinin elemanlarını büyükten küçüğe olacak şekilde ekrana yazdırın.
+        // Her iki dizinin eleman sayısını ve ortalamasını ekrana yazdırın.
+
+
+        int n = 20;
+        ArrayList asalSayilar = new ArrayList();
+        ArrayList asalolmayanSayilar = new ArrayList();
+
+
+    }
+}
