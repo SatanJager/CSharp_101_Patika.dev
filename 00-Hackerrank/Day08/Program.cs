3a47690dcaa00cc0881e677a2e0685803951155e
Taylan Alp Muhur <satanjager@gmail.com>
2026-05-21 17:08:46 +0200

    C# devam

---
+++ 00-Hackerrank/Day08/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Day08;
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+
+        int n = int.Parse(Console.ReadLine());
+
+        var telBook = new Dictionary<string, string>();  // int kullanılmadı çünkü telefon numarası 0 ile başlayabilir ve bu durumda int olarak saklanamaz. veya çok büyük olabilir.
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            string name = input[0];
+            string phoneNumber = input[1];
+
+            telBook.Add(name, phoneNumber);
+
+            //telBook[name] = phoneNumber;  // Dictionary'de aynı anahtar varsa, yeni değeri atar. Yoksa yeni bir anahtar-değer çifti ekler.
+        } 
+
+        string query;
+        while ((query = Console.ReadLine()) != null)
+        {
+            if (telBook.ContainsKey(query))
+            {
+                Console.WriteLine(query + "=" + telBook[query]);
+            }
+            else
+            {
+                Console.WriteLine("Not found");
+            }
+        }
+        
+    }
+}
